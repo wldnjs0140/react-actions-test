7e87945d1c824eee452318b7d4b4c7f1324f398d
wldnjs0140 <wldnjs0140@naver.com>
2024-11-17 23:11:04 +0900

    App.jsx_modify_add_import_vite

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { ghPages } from 'vite-plugin-gh-pages';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), ghPages()],
-  base: 'react-actions-test',
+  base: '/react-actions-test/',
+  esbuild: {
+    loader: 'jsx', // .js 파일에서 JSX 처리
+    include: /src\/.*\.js$/, // src 디렉토리 내 .js 파일을 JSX로 처리
+  },
 });
